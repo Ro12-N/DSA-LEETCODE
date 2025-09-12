dbaf49e9ea5a5b308a76bae08cd085e1fae32157
Rohan Naidu <rohannaidu940@gmail.com>
2025-09-12 10:30:38 -0500

    inital commit

---
+++ 2025/SEPTEMBER/vowelsGameInString.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        for(auto i:s){
+            if(i=='a'||i=='e'||i=='o'||i=='i'||i=='u') return true;
+        }
+        return false;
+    }
+};
