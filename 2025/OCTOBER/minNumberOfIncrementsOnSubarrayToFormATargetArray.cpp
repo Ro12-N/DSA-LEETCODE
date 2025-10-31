49507d8a24ec617cc900f7e5e25050c5941c386d
Rohan Naidu <rohannaidu940@gmail.com>
2025-10-30 22:46:18 -0500

    initial commit

---
+++ 2025/OCTOBER/minNumberOfIncrementsOnSubarrayToFormATargetArray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minNumberOperations(vector<int>& target) {
+        int ans=0;
+        int prev=0;
+        for(auto i:target){
+            if(prev<i){
+                ans+=(i-prev);
+            }
+            prev=i;
+        }
+        return ans;
+    }
+};
