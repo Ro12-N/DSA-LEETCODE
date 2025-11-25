0f69d83d4b18cf9dd27b16f28d0edb1016ea6180
Ro12-N <rohannaidu940@gmail.com>
2025-11-25 10:37:38 +0530

    initial commit

---
+++ 2025/NOVEMBER/smallestIntegerDivisibleByK.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int smallestRepunitDivByK(int k) {
+        if(k%2==0||k%5==0) return -1;
+        int cnt=0;
+        long long curr=1;
+        while(curr<=LLONG_MAX){
+            cnt++;
+            if(curr%k==0) return cnt;
+            curr=((curr*10)+1)%k;
+        }
+        return -1;
+    }
+};
