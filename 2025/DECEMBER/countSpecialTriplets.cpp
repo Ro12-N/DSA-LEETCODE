012f8b3accfc72f35a012e765277f15062238e5c
Ro12-N <rohannaidu940@gmail.com>
2025-12-09 19:39:31 +0530

    initial commit

--- 2025/DECEMBER/countSpecialTriplets.cpp
+++ 2025/DECEMBER/countSpecialTriplets.cpp
@@ -15,3 +15,23 @@ public:
 
     }
 };
+
+
+// class Solution {
+// public:
+//     int specialTriplets(vector<int>& nums) {
+//         unordered_map<int,int>mpi,mpj;
+//         int ans=0;
+//         int mod=1e9+7;
+//         for(auto i:nums){
+//             if(i%2==0){
+//                 ans=(ans+(1LL*mpj[i/2]))%mod;
+//             }
+//             mpj[i]=(mpj[i]+mpi[i*2])%mod;
+//             mpi[i]++;
+//         }
+
+
+//         return ans;
+//     }
+// };
