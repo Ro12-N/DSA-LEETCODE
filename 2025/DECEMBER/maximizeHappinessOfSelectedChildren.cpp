e8c5c1d6ed6f9c8d77555971ecae0e3e933a1e05
Ro12-N <rohannaidu940@gmail.com>
2025-12-25 08:22:20 +0530

    initial commit

---
+++ 2025/DECEMBER/maximizeHappinessOfSelectedChildren.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        long long ans=0;
+        sort(happiness.rbegin(),happiness.rend());
+        for(int i=0;i<k;i++){
+            ans+=max(0,happiness[i]-i);
+        }
+        return ans;
+    }
+};
+// Time Complexity: O(N log N) due to sorting
+// Space Complexity: O(1)
