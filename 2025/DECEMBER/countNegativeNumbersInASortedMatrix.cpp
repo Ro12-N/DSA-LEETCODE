ac7cad4f47cdc729e5ee712cb4e43426cb2a0592
Ro12-N <rohannaidu940@gmail.com>
2025-12-28 09:25:46 +0530

    initial commit

---
+++ 2025/DECEMBER/countNegativeNumbersInASortedMatrix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        int i=0;
+        int j=m-1;
+        int ans=0;
+        while(i<n && j>=0){
+           if(grid[i][j]<0){
+              ans+=n-i;
+              j--;
+           }else i++;
+        }
+
+
+        return ans;
+    }
+};
+// Time Complexity: O(N + M) where N is number of rows and M is number of columns
+// Space Complexity: O(1)
