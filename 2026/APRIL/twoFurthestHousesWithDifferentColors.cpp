f233c77ea2730b1a8fe3d64b8ebb6126f50abef9
ro12-n <rohannaidu940@gmail.com>
2026-04-21 21:50:31 +0530

    DSU

---
+++ 2026/APRIL/twoFurthestHousesWithDifferentColors.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+        int n=colors.size();
+        int i=0,j=n-1;
+        int ans=0;
+        while(i<j){
+            if(colors[i]!=colors[j]) ans=max(ans,j-i);
+             j--;
+        }
+        i=0;
+        j=n-1;
+        while(i<j){
+            if(colors[i]!=colors[j]) ans=max(ans,j-i);  
+            i++;
+        }
+        return ans;
+    }
+};
