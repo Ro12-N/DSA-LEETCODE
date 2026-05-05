3d59e9fb3981b2a23ef59d5575aaca3cf0f92f42
ro12-n <rohannaidu940@gmail.com>
2026-05-05 07:50:01 +0530

    Reverse Linked List

---
+++ 2026/MAY/rotateImage.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=i;j<m;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+    }
+};
+// TC : O(n^2) where n is the number of rows/columns in the input matrix
+// SC : O(1) as we are using only constant space to store the temporary variables
